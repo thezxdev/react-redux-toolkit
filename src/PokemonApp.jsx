069a68d25b19a3168c5d0efb37f8ffde814333cd
thezxdev <thezxdev@gmail.com>
2022-08-21 15:18:56 -0700

    Usar valores del store de pokemon en la pantalla de PokemonApp

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,26 +1,38 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
   
+  const { page, pokemons, isLoading } = useSelector( state => state.pokemons );
   const dispatch = useDispatch();
 
+  // Mandar a llamar a la función de getPokemons cuando se cargue el componente
   useEffect(() => {
     dispatch( getPokemons() );
-  }, [])
+  }, [] );
   
 
   return (
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: { isLoading ? 'True' : 'False' }</span>
 
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {
+          pokemons.map( ({ name }) => (
+            <li key={ name }>{ name }</li>
+          ))
+        }
       </ul>
+
+      <button
+        disabled={ isLoading }
+        onClick={ () => dispatch( getPokemons( page ) ) }
+      >
+        Next
+      </button>
     </>
   )
 }
